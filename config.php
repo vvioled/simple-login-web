7a6d533c163d4478d55cbbc9e460233f71e27756
Dwiky Rizky Ananditya <kyzsuki@gnuweeb.org>
2023-12-12 12:44:21 +0700

    Handle PDOException

--- config.php
+++ config.php
@@ -6,4 +6,13 @@ $user = "root";
 $pass = "";
 
 $dsn = "mysql:host=$host;dbname=$db";
-$pdo = new PDO($dsn, $user, $pass);
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo "<h1>500 - Internal Server Error</h1>";
+    exit(0);
+}
